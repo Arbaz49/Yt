3ff812b859dd434121966b49311c835c80587576
Arbaz <arbaz24.3.20@gmail.com>
2023-02-21 19:20:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// import "./App.css";
+// import LoadingBar from 'react-top-loading-bar';
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/pages/Videopage.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import "./Videopage.css";
+
+
+
+const Videopage = () => {
+  let { id } = useParams();
+
+
+  return (
+    <div >
+
+      <div className="vidContainer">
+        <iframe
+        style={{width: '98vw', height: "98vh",margin:"0px"}}
+          // width="100%"
+          // height="100%"
+          src={`https://www.youtube.com/embed/${id}`}
+          title="YouTube video player"
+          
+          allowFullscreen
+        ></iframe>
+      {/* <i className="fa-sharp fa-solid fa-thumbs-up likebtn" onClick={increaseLike} >{likecounter}</i> */}
+      </div>
+
+      {/* <div style={{width:"38%",hight:"100vh",borderLeft:"2px solid red"}}>
+      <div style={{width:"100%",border:"1px solid black"}}>
+        h
+      </div>
+      </div> */}
+     
+    </div>
+  );
+};
+
+export default Videopage;
